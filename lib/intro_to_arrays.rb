afef8343f7ad4c573ab404f253b4599af6a2e925
Joshua Crum <joshua.crum@gmail.com>
2019-04-28 17:23:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,10 +1,10 @@
 def instantiate_new_array
-  @new_array = instantiate_new_array
+  @my_new_array = instantiate_new_array
 end
 
 def array_with_two_elements
   array_with_two_elements = ["dog", 1]
-  @two_array =  array_with_two_elements
+  @my_two_array =  array_with_two_elements
 end
 
 def array(element)
