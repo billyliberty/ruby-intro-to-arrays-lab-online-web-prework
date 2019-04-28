9029301c76108646746cfebad24667eed5eb641e
Joshua Crum <joshua.crum@gmail.com>
2019-04-28 16:39:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,8 @@ def array_with_two_elements
   array_with_two_elements = ["dog", 1]
   @two_array =  array_with_two_elements
 end
+
+def array(element)
+  @array = ["dog", "cat", "mouse", "shark"]
+  element = array[0]
+end
