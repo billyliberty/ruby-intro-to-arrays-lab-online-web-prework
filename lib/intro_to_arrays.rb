3d7483dd8638c00d449c173cf74d89b42b4f3293
Joshua Crum <joshua.crum@gmail.com>
2019-04-28 17:28:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,13 +1,24 @@
 def instantiate_new_array
+  instantiate_new_array = []
   @my_new_array = instantiate_new_array
 end
 
-def array_with_two_elements
-  array_with_two_elements = ["dog", 1]
-  @my_two_array =  array_with_two_elements
+ def array_with_two_elements
+  array_with_two_elements = [1, 2]
+  @my_two_array = array_with_two_elements
 end
 
-def first_element(array)
+ def first_element(my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  array = @taylor_swift.first
+  my_first_element = @taylor_swift.first
+end
+
+ def third_element(my_third_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = @taylor_swift[2]
+end
+
+ def last_element(my_last_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = @taylor_swift[-1]
 end
